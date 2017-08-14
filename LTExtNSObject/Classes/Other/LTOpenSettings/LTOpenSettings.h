2ff5994f8ada7f19fb7505ec9f0eef7ca20588af
yjpal <yl21ly@qq.com>
2017-08-14 10:21:30 +0800

    remove 注释

--- LTExtNSObject/Classes/Other/LTOpenSettings/LTOpenSettings.h
+++ LTExtNSObject/Classes/Other/LTOpenSettings/LTOpenSettings.h
@@ -19,24 +19,4 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-//
-/*
-extern NSString * const LTSettingsWifiURLString;//WIFI
-extern NSString * const LTSettingsLocationURLString;//定位服务
-extern NSString * const LTSettingsNotificationURLString;//通知
-extern NSString * const LTSettingsBluetoothURLString;//蓝牙
 
-extern NSString * const LTSettingsPhoneURLString;//电话
-extern NSString * const LTSettingsPhotosURLString;//相册
-extern NSString * const LTSettingsAirplaneModeOnURLString;//设置？？
-extern NSString * const LTSettingsInternetTetheringURLString;//热点
-extern NSString * const LTSettingsSettingURLString;//设置
-extern NSString * const LTSettingsFaceTimeURLString;//fance time
-extern NSString * const LTSettingsGeneralURLString;//设置通用
-extern NSString * const LTSettingsAboutURLString;//关于本机
-extern NSString * const LTSettingsAutoLockURLString;//自动锁定
-extern NSString * const LTSettingsKeyboardURLString;//键盘
-extern NSString * const LTSettingsAccessibilityURLString;//辅助功能
-extern NSString * const LTSettingsDateTimeURLString;//日期时间
-extern NSString * const LTSettingsProfileURLString;//描述文件与设备管理
-*/
--- LTExtNSObject/Classes/Other/LTOpenSettings/LTOpenSettings.m
+++ LTExtNSObject/Classes/Other/LTOpenSettings/LTOpenSettings.m
@@ -42,32 +42,4 @@ void LTOpenSettingsURLString(NSString *urlString){
         [[UIApplication sharedApplication] openURL:url];
     }
 }
-/*
-NSString * const LTSettingsWifiURLString        = @"prefs:root=WIFI";//WIFI
-NSString * const LTSettingsLocationURLString    = @"prefs:root=LOCATION_SERVICES";//定位服务
-NSString * const LTSettingsNotificationURLString   = @"prefs:root=NOTIFICATIONS_ID";//通知
-
-NSString * const LTSettingsPhoneURLString       = @"prefs:root=Phone";//电话
-
-NSString * const LTSettingsPhotosURLString      = @"prefs:root=Photos";//相册
-NSString * const LTSettingsAirplaneModeOnURLString   = @"prefs:root=AIRPLANE_MODE";//设置？？
-
-
-//NSString * const LTSettingsBrightnessURLString  = @"prefs:root=Brightness";
-NSString * const LTSettingsInternetTetheringURLString     = @"prefs:root=INTERNET_TETHERING";
-NSString * const LTSettingsSettingURLString = @"prefs:";设置
-NSString * const LTSettingsFaceTimeURLString    = @"prefs:root=FACETIME";//fance time
-
-NSString * const LTSettingsGeneralURLString     = @"prefs:root=General";//设置通用
-NSString * const LTSettingsAboutURLString       = @"prefs:root=General&path=About";//关于本机
-NSString * const LTSettingsAutoLockURLString    = @"prefs:root=General&path=AUTOLOCK";//自动锁定
-NSString * const LTSettingsKeyboardURLString    = @"prefs:root=General&path=Keyboard";//键盘
-NSString * const LTSettingsAccessibilityURLString   = @"prefs:root=General&path=ACCESSIBILITY";//辅助功能
-NSString * const LTSettingsBluetoothURLString   = @"prefs:root=Bluetooth";//蓝牙
-//NSString * const LTSettingsNetworkURLString     = @"prefs:root=Network";
-NSString * const LTSettingsDateTimeURLString    = @"prefs:root=General&path=DATE_AND_TIME";//日期时间
-
-NSString * const LTSettingsProfileURLString   = @"prefs:root=General&path=ManagedConfigurationList";//描述文件与设备管理
-*/
-
 
